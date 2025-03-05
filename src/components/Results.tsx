2d029f091fa9b5fbd59aaa0bc5c7d3e9dbf50a60
Federico Redolfo <redolfofederico@gmail.com>
2025-03-05 19:45:09 -0300

    new change

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -17,7 +17,10 @@ export const Results = ({data}: {data?: CommentWithId[]}) => {
                         className={`block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100`}>
                             <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900'> {comment.title} </h5>
                             <p className='font-normal text-gray-700'> {comment.message} </p>
-                            <button onClick={() => handleDelete(comment.id)} className='mt-4 px-12 text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm py-2.5 text-center mr-2 mb-2'>
+                            <button
+                                disabled={deleteCommentMutation.isPending}  
+                                onClick={() => handleDelete(comment.id)} 
+                                className='mt-4 px-12 text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm py-2.5 text-center mr-2 mb-2'>
                                 {deleteCommentMutation.isPending ? 'Eliminando...' : 'Eliminar'}
                             </button>
                     </article>
